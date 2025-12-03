bd07550a632ce412ecea606dd17bef6ebfa44e12
Vojtěch Klos <klosv@crs-company.cz>
2025-12-03 19:12:34 +0100

    sequential loading of images in gallery

--- components/gallery/Gallery.js
+++ components/gallery/Gallery.js
@@ -1,18 +1,19 @@
 oc.registerControl('gallery', class extends oc.ControlBase {
     init() {
         this.images = this.element.querySelectorAll('.Gallery-imageHolder img');
+        this.imagesToLoad = Array.from(this.element.querySelectorAll('.Gallery-imageHolder img'));
         this.columns = this.element.querySelectorAll('.Gallery-column');
         this.wrapper = this.element.querySelector('.Gallery-wrapper');
         this.maxLoadImages = 99999;
 
         this.unloadedImages = [];
-        this.loadedImages = 0;
+        this.loadedImagesNum = 0;
     }
 
     connect() {
-        if (this.images.length < this.columns.length * 2) {
+        if (this.images.length > this.columns.length * 2) {
             this.wrapper.classList.remove('Gallery-wrapper--fewImages');
-            this.maxLoadImages = this.columns.length;
+            this.maxLoadImages = this.columns.length * 2;
         } else {
             this.wrapper.classList.add('Gallery-wrapper--fewImages');
             this.maxLoadImages = 99999;
@@ -24,39 +25,52 @@ oc.registerControl('gallery', class extends oc.ControlBase {
     }
 
     loadImages() {
-        this.loadedImages = 0;
-        this.images.forEach((image) => {
-            image.addEventListener('load', () => {
-                if (this.loadedImages >= this.maxLoadImages) {
-                    this.unloadedImages.push(image);
-                    return;
-                }
-                this.loadedImages++;
-                this.distributeImage(image);
-            });
+        if (this.imagesToLoad.length === 0) {
+            return;
+        }
 
-            if (image.complete) {
-                if (this.loadedImages >= this.maxLoadImages) {
-                    this.unloadedImages.push(image);
-                    return;
-                }
-                this.loadedImages++;
-                this.distributeImage(image);
+        const image = this.imagesToLoad.shift();
+
+        image.addEventListener('load', () => {
+            if (this.loadedImagesNum >= this.maxLoadImages) {
+                this.unloadedImages.push(image);
+                this.loadImages();
+                return;
             }
+
+            this.loadedImagesNum++;
+            this.distributeImage(image);
+            setTimeout(() => {
+                this.loadImages();
+            }, 100);
         });
+
+        if (image.complete) {
+            if (this.loadedImagesNum >= this.maxLoadImages) {
+                this.unloadedImages.push(image);
+                this.loadImages();
+                return;
+            }
+
+            this.loadedImagesNum++;
+            this.distributeImage(image);
+            setTimeout(() => {
+                this.loadImages();
+            }, 100);
+        }
     }
 
     loadMoreImages() {
-        const imagesToLoad = this.unloadedImages;
-        imagesToLoad.forEach((image) => {
-            this.loadedImages++;
-            this.distributeImage(image);
-        });
+        console.log(this.unloadedImages);
+        this.imagesToLoad = this.unloadedImages;
+        this.maxLoadImages = 99999;
+        this.loadImages();
 
         this.wrapper.classList.add('Gallery-wrapper--allImages');
     }
 
     distributeImage(image) {
+        console.log('distributeImage', image);
         let shortestColumn = this.columns[0];
 
         this.columns.forEach((column) => {
@@ -71,8 +85,7 @@ oc.registerControl('gallery', class extends oc.ControlBase {
 
         shortestColumn.appendChild(wrapper);
 
-        if (this.loadedImages === this.maxLoadImages) {
-            console.log('asdf')
+        if (this.loadedImagesNum === this.maxLoadImages) {
             // tallest column height
             let tallestHeight = 0;
             this.columns.forEach((column) => {
